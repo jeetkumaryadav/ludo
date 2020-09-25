b4d227a499a46e943ef11afb10acdadd3e052b04
jeetkumaryadav <jeetkishoreyadav@gmail.com>
2020-09-25 21:10:25 +0530

    UC2 Done

--- ludo.java
+++ ludo.java
@@ -2,4 +2,6 @@ public class ludo{
 public static void main(String[] args){
 int pos=0;
 System.out.println("Welcome to Snake and Ladder Game\nYour initial position is "+pos);
+int diceNum =(int)Math.floor(Math.random()*10)%6+1;
+System.out.println("Dice Number: "+diceNum);
 }}
